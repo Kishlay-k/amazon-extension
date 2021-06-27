488648488678f23c88e66d088dc55b6a801be87f
Kishlay <KISHLAYKUMAR@IITG.AC.IN>
2021-06-27 16:22:27 +0530

    Completed Task 4

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -5,10 +5,22 @@ const morgan = require("morgan");
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.urlencoded({extended: true}));
 app.get("/",function(req,res){
   res.send("Hello World!")
 })
+
+app.post("/test",function(req,res){
+    console.log(req.body)
+    res.send("Success")
+})
+
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
 });
--- Extension/background.js
+++ Extension/background.js
@@ -1,1 +1,10 @@
 console.log("Hello World from console");
+
+const data={
+    name:"Kishlay",
+    id:12345
+}
+
+$.post("http://localhost:3000/test",data,function(data,status){
+    console.log(data);
+})
